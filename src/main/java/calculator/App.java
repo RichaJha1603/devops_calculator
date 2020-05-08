637414d338749d5b51b37946d63ac70f248d7b9d
RichaJha1603 <58663497+RichaJha1603@users.noreply.github.com>
2020-05-08 20:57:56 +0530

    Update App.java

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -13,8 +13,8 @@ public class App
         {
             System.out.println("Option Menu");
             System.out.println("");
-            System.out.println("1) Addition");
-            System.out.println("2) Subtraction");
+            System.out.println("1) Add");
+            System.out.println("2) Sub");
             System.out.println("3) Multiplication");
             System.out.println("4) Division");
             System.out.println("5) Exit");
