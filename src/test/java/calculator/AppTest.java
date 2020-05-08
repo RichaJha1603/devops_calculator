4e89226b3887fe2e874b0858c6753cc65c032bc3
RichaJha1603 <58663497+RichaJha1603@users.noreply.github.com>
2020-05-09 01:39:10 +0530

    Update AppTest.java

--- src/test/java/calculator/AppTest.java
+++ src/test/java/calculator/AppTest.java
@@ -33,7 +33,7 @@ public class AppTest {
 	        int a = 15;
 	        int b = 20;
 	        int expectedResult = 35;
-	        int result = Calculator.add(a, b);
+	        int result = App.add(a, b);
 	        Assert.assertEquals(expectedResult, result);;
 	    }
 	    
@@ -42,7 +42,7 @@ public class AppTest {
 	        int a = 25;
 	        int b = 20;
 	        int expectedResult = 5;
-	        int result = Calculator.sub(a, b);
+	        int result = App.sub(a, b);
 	        Assert.assertEquals(expectedResult, result);;
 	    }
 	    
@@ -51,7 +51,7 @@ public class AppTest {
 	    	int a = 10;
 	    	int b = 25;
 	    	int expectedResult = 250;
-	    	int result = Calculator.mul(a, b);
+	    	int result = App.mul(a, b);
 	        Assert.assertEquals(expectedResult, result);;
 	    }
 	    
@@ -60,14 +60,14 @@ public class AppTest {
 	        int a = 56;
 	        int b = 10;
 	        int expectedResult = 5;
-	        int result = Calculator.div(a, b);
+	        int result = App.div(a, b);
 	        Assert.assertEquals(expectedResult, result);
 	    }
 	    @Test(expected = ArithmeticException.class)
 	    public void testDivideByZero() {
 	        int a = 15;
 	        int b = 0;
-	        Calculator.div(a, b);
+	        App.div(a, b);
 	    }
 
 }
